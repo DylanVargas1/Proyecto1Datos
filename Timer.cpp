e0d01079abacf7eab7c5ec1647313829812a977f
DylanVargas1 <dylan.vargas.azofeifa@est.una.ac.cr>
2026-09-17 02:17:18 -0600

    actualizacion archivos

--- Timer.cpp
+++ Timer.cpp
@@ -8,8 +8,10 @@ void Timer::reiniciar() {
     inicio = std::chrono::high_resolution_clock::now();
 }
 
+
+
 float Timer::segundosTranscurridos() const {
-    auto ahora = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> duracion = ahora - inicio;
-    return duracion.count();
+	auto ahora = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> duracion = ahora - inicio;
+	return duracion.count();
 }
